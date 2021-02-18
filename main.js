143dfd88dc3503b64879e25c9a913a845e45ad14
Samaelish <semnd007@gmail.com>
2021-02-18 04:19:19 +0300

    favicon, scroll reveal library animation effects, styling

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ function toggleMenu() {
     menuToggle.classList.toggle('active');
     menu.classList.toggle('active');
 }
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 750,
+    // reset: true for additional animation
+});
+
+sr.reveal(` .servicesBx,
+            .workBx, .goodsBx,
+            .contactInfoBx, .box,
+            .textBx, .contentBx,
+            .footer__link, .social-list__link`, {
+    interval: 200
+})
